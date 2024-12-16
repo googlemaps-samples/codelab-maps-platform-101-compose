2cd7ef38411912d06c752d5456375b182c1dfd3a
dkhawk <107309+dkhawk@users.noreply.github.com>
2024-12-16 14:24:18 -0700

    core: Updates kotlin, gradle, and libraries.  Converted to version catalog.

--- solution/build.gradle.kts
+++ solution/build.gradle.kts
@@ -3,17 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 
     alias(libs.plugins.secrets.gradle.plugin) apply false
-
-//    id("com.android.application") version "8.7.3" apply false
-//    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-
-    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
-
-//buildscript {
-//    dependencies {
-//        classpath(libs.secrets.gradle.plugin)
-//    }
-//}
