7d033ed0d5994c56b2479c76f5abcd25541a7e21
dkhawk <107309+dkhawk@users.noreply.github.com>
2024-12-16 12:39:44 -0700

    chore: version catalog, kotlin update, library versions updated
    Update to use version catalog
    Update to Kotlin 2.0.21
    Update numerous libraries

--- solution/build.gradle.kts
+++ solution/build.gradle.kts
@@ -1,12 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+    alias(libs.plugins.secrets.gradle.plugin) apply false
+
+//    id("com.android.application") version "8.7.3" apply false
+//    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
-buildscript {
-    dependencies {
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
-}
+//buildscript {
+//    dependencies {
+//        classpath(libs.secrets.gradle.plugin)
+//    }
+//}
